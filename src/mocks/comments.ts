cd780eb5e59d54b9c1c1d13cf02e146c059acb3b
Fanomezantsoa Herifiandry Marc Nico <ny.kalash@gmail.com>
2023-02-10 17:30:30 +0300

    feat: create comments mocks

---
+++ src/mocks/comments.ts
@@ -0,0 +1,6 @@
+export const comments = [
+  { id: 1, text: "lorem ipsum dolor sit amet, text one" },
+  { id: 2, text: "lorem ipsum dolor sit amet, text two" },
+  { id: 3, text: "lorem ipsum dolor sit amet, text three" },
+  { id: 4, text: "lorem ipsum dolor sit amet, text four" },
+];
