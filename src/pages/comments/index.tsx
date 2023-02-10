f217c18296719ee176c7a689700c0cacaaf87960
Fanomezantsoa Herifiandry Marc Nico <ny.kalash@gmail.com>
2023-02-10 18:29:19 +0300

    feat: post comment

--- src/pages/comments/index.tsx
+++ src/pages/comments/index.tsx
@@ -1,20 +1,38 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
-interface IComments {
+interface IComment {
   id: number;
   text: string;
 }
 
 function Comments() {
-  const [comments, setComments] = useState<IComments[]>([]);
-  const handleclick = async () => {
+  const [comments, setComments] = useState<IComment[]>([]);
+  const [comment, setComment] = useState<string>("");
+
+  const handleclick = async (): Promise<void> => {
     const response = await fetch("http://localhost:3000/api/comments");
-    const comments: IComments[] = await response.json();
+    const comments: IComment[] = await response.json();
     setComments(comments);
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setComment(e.target.value);
+  };
+
+  const addComment = async (): Promise<void> => {
+    await fetch("http://localhost:3000/api/comments", {
+      method: "POST",
+      body: JSON.stringify({ text: comment }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
   };
 
   return (
     <div>
+      <input type="text" value={comment} onChange={handleChange} />
+      <button onClick={addComment}>POST: Comment</button>
       <button onClick={handleclick}>GET: Comments</button>
       {comments.map((comment) => {
         return (
